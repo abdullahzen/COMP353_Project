165f276bc31c1ecb2e19f45ad507e33a83d5ed76
Omnia <ogomaa.concordia@gmail.com>
2019-12-02 14:38:08 -0500

    header re-added

--- scc/public/header.php
+++ scc/public/header.php
@@ -1,3 +1,22 @@
+<!--<div align="center">-->
+<!--    <h1>SCC 2019 Admin Panel</h1>-->
+<!--    <a href="index.php">Home</a>-->
+<!--    <a href="bank_information.php">Bank Information</a>-->
+<!--    <a href="index.php">Events</a>-->
+<!--    <a href="index.php">Event Group</a>-->
+<!--    <a href="index.php">Event Organization Participants</a>-->
+<!--    <a href="index.php">Groups</a>-->
+<!--    <a href="index.php">Group Members</a>-->
+<!--    <a href="index.php">Group Posts</a>-->
+<!--    <a href="index.php">Messages</a>-->
+<!--    <a href="index.php">Organizations</a>-->
+<!--    <a href="index.php">Posts</a>-->
+<!--    <a href="index.php">Post Comments</a>-->
+<!--    <a href="index.php">Roles</a>-->
+<!--    <a href="index.php">User Bank Information</a>-->
+<!--    <a href="index.php">User Roles</a>-->
+<!--</div>-->
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css">
 

8c915301d1c5b895d19e5707b6e5efdff199ddcf
Omnia <ogomaa.concordia@gmail.com>
2019-12-02 00:32:41 -0500

    added php code to register new user in database (needs to be reviewed)

--- scc/public/sign-up.php
+++ scc/public/sign-up.php
@@ -23,6 +23,18 @@
             <input type="email" class="form-control" id="exampleInputEmail">
         </div>
         <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name">
+        </div>
+        <div class="form-group">
+            <label for="name">Phone number</label>
+            <input type="number" class="form-control" id="phone-number">
+        </div>
+        <div class="form-group">
+            <label for="address">Address</label>
+            <input type="text" class="form-control" id="address">
+        </div>
+        <div class="form-group">
             <label for="exampleInputPassword">Password</label>
             <input type="password" class="form-control" id="exampleInputPassword">
         </div>
@@ -32,6 +44,66 @@
 </body>
 
 </html>
+
+<?php
+//session_start();
+//
+//// initializing variables
+//$name = "";
+//$email    = "";
+//$phone_number = "";
+//$address = "";
+//$password = "";
+//$errors = array();
+//
+//// connect to the database
+//$db = mysqli_connect('localhost', 'root', '', 'registration');
+//
+//// REGISTER USER
+//if (isset($_POST['users'])) {
+//    // receive all input values from the form
+//    $name = mysqli_real_escape_string($db, $_POST['username']);
+//    $email = mysqli_real_escape_string($db, $_POST['email']);
+//    $password = mysqli_real_escape_string($db, $_POST['password']);
+//    $address = mysqli_real_escape_string($db, $_POST['address']);
+//    $phone_number = mysqli_real_escape_string($db, $_POST['phone-number']);
+//
+//    // form validation: ensure that the form is correctly filled ...
+//    // by adding (array_push()) corresponding error unto $errors array
+//    if (empty($username)) { array_push($errors, "Username is required"); }
+//    if (empty($email)) { array_push($errors, "Email is required"); }
+//    if (empty($password)) { array_push($errors, "Password is required"); }
+//    }
+//
+//    // first check the database to make sure
+//    // a user does not already exist with the same username and/or email
+//    $user_check_query = "SELECT * FROM users WHERE name='$name' OR email='$email' LIMIT 1";
+//    $result = mysqli_query($db, $user_check_query);
+//    $user = mysqli_fetch_assoc($result);
+//
+//    if ($user) { // if user exists
+//        if ($user['username'] === $name) {
+//            array_push($errors, "Username already exists");
+//        }
+//
+//        if ($user['email'] === $email) {
+//            array_push($errors, "email already exists");
+//        }
+//    }
+//
+//    // Finally, register user if there are no errors in the form
+//    if (count($errors) == 0) {
+//        $password = md5($password);//encrypt the password before saving in the database
+//
+//        $query = "INSERT INTO users (name, email, password)
+//  			  VALUES('$name', '$email', '$password')";
+//        mysqli_query($db, $query);
+//        $_SESSION['name'] = $name;
+//        $_SESSION['success'] = "You are now logged in";
+//        header('location: index.php');
+//    }
+//}
+
 
 
 

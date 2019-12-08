a4eedbc3d51e406d3c2afbed5f2b51ca3c50a7f5
Nour El Natour <nour_elnatour@hotmail.com>
2019-12-07 19:15:37 -0500

    Implemented single event page

--- public/events.php
+++ public/events.php
@@ -24,10 +24,10 @@ try {
 <script type="text/javascript">
     window.location = "
     <?php
-        if ($_COOKIE['current_role'] === 'admin') {
+        if ($_COOKIE['current_role'] === 'admin' || $_COOKIE['current_role'] === 'manager') {
             echo "/read.php?table=" . $_GET['table'];
         }
-        if ($_COOKIE['current_role'] === 'manager' || $_COOKIE['current_role'] === 'participant') {
+        if ($_COOKIE['current_role'] === 'participant') {
             echo "/events.php";
         }
         ?>";
@@ -41,7 +41,7 @@ if (isset($_POST["submit"])) {
 //    if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
     try {
         delete('events', key($result[0]), $_POST["submit"]);
-        $success = "User successfully deleted";
+        $success = "Event successfully deleted";
         ?>
         <script type="text/javascript">
             window.location = "/events.php";
@@ -54,63 +54,73 @@ if (isset($_POST["submit"])) {
 ?>
 <?php
     include "header.php";
-//    var_dump($result);
 ?>
 <?php
     if (sizeof($result) > 0) {
 ?>
-<form method="post">
-    <input name="csrf" type="hidden" value="<?php echo ($_SESSION['csrf']); ?>">
-    <a href="create.php?table=<?php echo ($table) ?>">Create new entry for <?php echo ($table) ?></a>
-    <table align="right">
-        <thead>
-            <tr>
-                <?php foreach($result[0] as $key => $value){ ?>
-                    <th>
-                        <?php echo $key; ?>
-                    </th>
-                <?php } ?>
-                <?php
-                if($_COOKIE['current_role'] === 'admin' || $_COOKIE['current_role'] === 'manager') {
-                    echo "
+       <div class="content">
+            <form method="post">
+                <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
+                <div align="right">
+                    <a class="create_new" href="create.php?table=<?php echo escape($table) ?>">Add a new Event</a>
+                </div>
+                <table class="table_template center" >
+                    <div>
+                        <h1 class="center">Manage Events</h1>
+                    </div>
+                    <thead>
+                    <tr>
+                        <?php foreach($result[0] as $key => $value){ 
+                            if ($key === 'event_ID'){
+                                continue;
+                            }
+                            ?>
+                            
+                            <th>
+                                <?php echo $key; ?>
+                            </th>
+                        <?php } ?>
                         <th>Edit</th>
                         <th>Delete</th>
-                    ";
-                }
-                ?>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $index = 0;
-            foreach ($result as $key => $value) { ?>
-                <tr>
+                    </tr>
+                    </thead>
+                    <tbody>
                     <?php
-                        foreach ($result[$index] as $key => $value) {
+                    $index = 0;
+                    foreach ($result as $key => $value) { ?>
+                        <tr>
+                            <?php
+                            foreach ($result[$index] as $key => $value) {
+                                if ($key === 'event_ID'){
+                                    $event_ID = $value;
+                                    continue;
+                                }
+                                ?>
+                                <?php if ($key === 'name'){?>
+
+                                    <td><a href="event.php?id=<?php echo escape($event_ID)?>"><?php echo $result[$index][$key]; ?></a></td>
+                                <?php 
+                                } else {?>
+                                <td><?php echo $result[$index][$key]; ?></td>
+                                <?php } ?>
+                                <?php
+                            }
+                            ?>
+                            <td><a class="edit_button" href="update.php?table=<?php echo escape($table) ?>
+                            &key=<?php echo escape(key($result[$index])) ?>&id=<?php echo escape($result[$index][key($result[$index])]); ?>">Edit</a></td>
+                            <td><button type="submit" name="submit" value="<?php echo escape($result[$index][key($result[$index])]); ?>">Delete</button></td>
+                        </tr>
+                        <?php
+                        $index++;
+                    }
                     ?>
-                            <td><?php echo $result[$index][$key]; ?></td>
-                    <?php
-                        }
-                    ?>
-                    <?php
-                        if($_COOKIE['current_role'] === 'admin' || $_COOKIE['current_role'] === 'manager') {
-                    ?>
-                            <td><a href="update.php?table=events&key=<?php echo (key($result[$index])) ?>&id=<?php echo ($result[$index][key($result[$index])]); ?>">Edit</a></td>
-                            <td><button type="submit" name="submit" value="<?php echo ($result[$index][key($result[$index])]); ?>">Delete</button></td>
-                    <?php
-                        }
-                    ?>
-                </tr>
-            <?php
-                $index++;
-            }
-            ?>
-        </tbody>
-    </table>
-</form>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+
+
 <?php } else { ?>
-    <div align="center">
-        <blockquote>No events found.</blockquote>
-    </div>
+    <blockquote>No results found.</blockquote>
 <?php
 } ?>
